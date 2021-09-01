a3c3a19162902b9d218eff15f90a81d22fa0b1f7
Eric Sanchez <ericfsod@gmail.com>
2021-08-31 23:01:36 -0400

    Completed Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'plants/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+        get '/plants', to: 'plants#index'
+        get '/plants', to: 'plants#show'
+        post "/plants", to: 'plants#create'
+      end
